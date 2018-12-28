7a2961d74cd10318d5a69211f4c062852341b26e
Lou Zuniga <43398948+louzuniga@users.noreply.github.com>
2018-12-27 22:22:40 -0800

    Updated all files to complete MVP implementation. Currently a working app

---
+++ README.md
@@ -0,0 +1,5 @@
+RANDOM! 
+
+Are you bored or simply browsing around and looking for something to do? Let RANDOM provide you some random ideas and show you some options. 
+
+Click the the RANDOM button and it will retrieve a random idea.
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function getRandomActivity() {
+    fetch ('https://www.boredapi.com/api/activity/')
+    .then (activity => activity.json())
+    .then(activityJson => displayRandomActivity(activityJson))
+    .catch(err => {
+        $('#js-error-message').text(`Uh oh, not cool: ${err.message}`);
+    });
+}
+
+function getRandomAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(advice => advice.json())
+    .then(adviceJson => displayRandomAdvice(adviceJson))
+    .catch(err => {
+        $('#js-error-message').text(`Uh oh, not cool:${err.message}`);
+    });
+}
+
+function getRandomName() {
+    fetch('https://uinames.com/api/')
+    .then(name => name.json())
+    .then(nameJson => displayRandomName(nameJson.name))
+    .catch(err => {
+        $('#js-error-message').text(`Uh oh, Not cool: ${err.message}`);
+    });
+}
+
+function displayRandomActivity(activityJson) {
+    emptyAndRemove();
+     $('#results-list').append(
+        $(`<p>Ponder about this random activity:<br><b>${activityJson.activity}</b></p>`)
+    );
+ }
+
+ function displayRandomAdvice(adviceJson) {
+     emptyAndRemove();
+     $('#results-list').append(
+         $(`<p>Here's advice:<br><b>${adviceJson.slip.advice}</b></p>`)
+    );console.log(adviceJson.slip.advice);
+ }
+
+ function displayRandomName(nameJson) {
+     emptyAndRemove();
+     $('#results-list').append(
+         $(`<p>What do you think of this name:<br><b>${nameJson}</b></p>`)
+     );console.log(nameJson);
+ }
+
+ function emptyAndRemove() {
+    $('#results-list').empty();
+    $('#results').removeClass('hidden');
+ }
+
+function watchForm () {
+  $('form').submit(event => {
+    event.preventDefault();
+  });
+    $('form').on('click', '.random-activity',
+        function () { 
+         return getRandomActivity();
+        });
+    $('form').on('click', '.random-advice',
+        function() {
+            return getRandomAdvice();
+        });
+    $('form').on( 'click', '.random-name', 
+        function() {
+            return getRandomName();
+        });
+}
+
+$(watchForm);
