bdcd1aa2da5dca3d8ee3e6d08471137612f1ba34
Lou Zuniga <43398948+louzuniga@users.noreply.github.com>
2018-12-30 23:44:36 -0800

    modified js for next button functionality

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 'use strict';
 
+const apiKeyCat = '9807a3e1-36d8-4699-90f7-bc2b5e9fb0a4';
+
+function getRandomCat () {
+    const url = 'https://api.thecatapi.com/v1/images/search'
+
+    const options = {
+        headers: new Headers ({
+            "x-api-key": apiKeyCat}) 
+    }; 
+
+   fetch(url, options)
+   .then(cat => {
+       if(cat.ok) {
+           return cat.json(); 
+       }
+       throw new Error(cat.statusText);
+   })
+   .then(catJson => displayRandomCat(catJson))
+   .catch(err => {
+        $('#js-error-message').text(`Uh oh, Not cool: ${err.message}`);
+   });
+}
+
+function getRandomDog() {
+    fetch ('https://dog.ceo/api/breeds/image/random')
+    .then(dog => {
+        if(dog.ok) {
+            return dog.json();
+        }
+        throw new Error(dog.statusText);
+    })
+    .then(dogJson => displayRandomDog(dogJson))
+    .catch(err => {
+         $('#js-error-message').text(`Uh oh, Not cool: ${err.message}`);
+    });
+}
+
 function getRandomActivity() {
     fetch ('https://www.boredapi.com/api/activity/')
     .then (activity => activity.json())
@@ -27,52 +64,21 @@ function getRandomAdvice() {
 //     });
 // }
 
-function getRandomDog() {
-    fetch ('https://dog.ceo/api/breeds/image/random')
-    .then(dog => {
-        if(dog.ok) {
-            return dog.json();
-        }
-        throw new Error(dog.statusText);
-    })
-    .then(dogJson => displayRandomDog(dogJson))
-    .catch(err => {
-         $('#js-error-message').text(`Uh oh, Not cool: ${err.message}`);
-    });
-}
-
-const apiKeyCat = '9807a3e1-36d8-4699-90f7-bc2b5e9fb0a4';
-
-function getRandomCat () {
-    const url = 'https://api.thecatapi.com/v1/images/search'
-
-    const options = {
-        headers: new Headers ({
-            "x-api-key": apiKeyCat}) 
-    }; 
-
-   fetch(url, options)
-   .then(cat => {
-       if(cat.ok) {
-           return cat.json(); 
-       }
-       throw new Error(cat.statusText);
-   })
-   .then(catJson => displayRandomCat(catJson))
-   .catch(err => {
-        $('#js-error-message').text(`Uh oh, Not cool: ${err.message}`);
-   });
-}
-
 function displayRandomActivity(activityJson) {
     emptyAndRemove();
      $('#results-list').append(
         $(`<p>Ponder about this activity:<br><strong>${activityJson.activity}</strong></p>`)
     );
+    $('.next').mouseup(function() {
+        $('.random-activity').click();
+    });
  }
 
  function displayRandomAdvice(adviceJson) {
      emptyAndRemove();
+     $('.next').mouseup(function() {
+        $('.random-advice').click();
+    });
      $('#results-list').append(
          $(`<p>Here's advice:<br><strong>${adviceJson.slip.advice}</strong></p>`)
     );
@@ -86,17 +92,23 @@ function displayRandomActivity(activityJson) {
 //  }
 
  function displayRandomDog(dogJson) {
-     emptyAndRemove();
-     $('#results-list').append(
-         $(`<img class="image" src="${dogJson.message}" alt="random dog image">`)
-     );
+    emptyAndRemove();
+    $('#results-list').append(
+        $(`<img class="image" src="${dogJson.message}" alt="random dog image">`)
+    );
+    $('.next').click(function() {
+        $('.random-dog').click();
+    });
  }
 
  function displayRandomCat(catJson) {
-     emptyAndRemove();
-     $('#results-list').append(
+    emptyAndRemove();
+    $('#results-list').append(
         $(`<img class="image" src="${catJson[0].url}" alt="random dog image">`)
-     );
+    );
+    $('.next').click(function() {
+        $('.random-cat').click();
+    });
  }
 
  function emptyAndRemove() {
@@ -110,16 +122,8 @@ function displayRandomActivity(activityJson) {
     $('#copyright').text(`Copyright \u00A9 ${d.getFullYear()} Lou Zuniga https://github.com/louzuniga`)
 }
 
-function nextButtom (activityJson, adviceJson) {
-    if($('.random-activity').click) { console.log(activityJson);
-         for(let i = 0; i < activityJson; i++)
-            return getRandomActivity(); 
-    }else if($('.random-advice').click) {
-        for(let i = 0; i < adviceJson; i++)
-        getRandomAdvice();
-    }else {
-        getRandomName();
-    }
+function nextButtom () { 
+
 }
 
 function watchForm () {
@@ -127,6 +131,12 @@ function watchForm () {
     event.preventDefault();
   });
   copyright();
+    $('.random-dog').on('click', () => {
+    return getRandomDog();
+    });
+    $('.random-cat').on('click', () => {
+    return getRandomCat();
+    });
     $('form').on('click', '.random-activity',
         function () { 
          return getRandomActivity();
@@ -139,20 +149,13 @@ function watchForm () {
     //     function() {
     //         return getRandomName();
     //     });
-    $('.random-dog').on('click', () => {
-        return getRandomDog();
-    });
-    $('.random-cat').on('click', () => {
-        return getRandomCat();
-    });
+   
     $('.previous').on('click',() => {
         location.reload();
         $('#results').addClass('hidden');
         $('#js-form').removeClass('hidden');
     });
-    $('.next').on('click', () => {
-        nextButtom();
-    });
+    
 }
 
 $(watchForm);
