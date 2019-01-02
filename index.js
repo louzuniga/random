f039c938180f4d6abcabff7c23ca1011c95da856
Lou Zuniga <43398948+louzuniga@users.noreply.github.com>
2019-01-01 21:50:14 -0800

    cleaned up js, html and css files

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const apiKeyCat = '9807a3e1-36d8-4699-90f7-bc2b5e9fb0a4';
+const urlCat = 'https://api.thecatapi.com/v1/images/search';
 
 function getRandomCat () {
-    const url = 'https://api.thecatapi.com/v1/images/search'
-
-    const options = {
+   const options = {
         headers: new Headers ({
             "x-api-key": apiKeyCat}) 
     }; 
 
-   fetch(url, options)
+   fetch(urlCat, options)
    .then(cat => {
        if(cat.ok) {
            return cat.json(); 
@@ -39,7 +38,12 @@ function getRandomDog() {
 
 function getRandomActivity() {
     fetch ('https://www.boredapi.com/api/activity/')
-    .then (activity => activity.json())
+    .then (activity => {
+        if(activity.ok) {
+            return activity.json();
+        }
+        throw new Error(activity.statusText);
+    })
     .then(activityJson => displayRandomActivity(activityJson))
     .catch(err => {
         $('#js-error-message').text(`Uh oh, not cool: ${err.message}`);
@@ -48,69 +52,48 @@ function getRandomActivity() {
 
 function getRandomAdvice() {
     fetch('https://api.adviceslip.com/advice')
-    .then(advice => advice.json())
+    .then (advice => {
+        if(advice.ok) {
+            return advice.json();
+        };
+    })
     .then(adviceJson => displayRandomAdvice(adviceJson))
     .catch(err => {
         $('#js-error-message').text(`Uh oh, not cool:${err.message}`);
     });
 }
 
-// function getRandomName() {
-//     fetch('https://uinames.com/api/')
-//     .then(name => name.json())
-//     .then(nameJson => displayRandomName(nameJson))
-//     .catch(err => {
-//         $('#js-error-message').text(`Uh oh, Not cool: ${err.message}`);
-//     });
-// }
-
 function displayRandomActivity(activityJson) {
-    $('#next-activity').removeClass('hidden');
     emptyAndRemove();
+    $('#next-activity').removeClass('hidden');
      $('#results-list').append(
         $(`<p>Ponder about this activity:<br><strong>${activityJson.activity}</strong></p>`)
-    );console.log('activity clicks')
+    );
+
  }
 
  function displayRandomAdvice(adviceJson) {
+    emptyAndRemove();
     $('#next-advice').removeClass('hidden');
-     emptyAndRemove();
-     $('.next').click(function() {
-        $('.random-advice').off();
-        getRandomAdvice();
-    });
      $('#results-list').append(
          $(`<p>Here's advice:<br><strong>${adviceJson.slip.advice}</strong></p>`)
     );
  }
 
-//  function displayRandomName(nameJson) {
-//      emptyAndRemove();
-//      $('#results-list').append(
-//          $(`<p>What do you think of this name from <em>${nameJson.region}</em>:<br><strong>${nameJson.name}</strong></p>`)
-//      );
-//  }
-
  function displayRandomDog(dogJson) {
-    $('#next-dog').removeClass('hidden');
     emptyAndRemove();
+    $('#next-dog').removeClass('hidden');
     $('#results-list').append(
         $(`<img class="image" src="${dogJson.message}" alt="random dog image">`)
     );
-    $('.next').click(function() {
-       getRandomDog();
-    });
  }
 
  function displayRandomCat(catJson) {
-    $('#next-cat').removeClass('hidden');
     emptyAndRemove();
+    $('#next-cat').removeClass('hidden');
     $('#results-list').append(
         $(`<img class="image" src="${catJson[0].url}" alt="random cat image">`)
     );
-    $('.next').click(function() {
-        $('.random-cat').click();
-    });
  }
 
  function emptyAndRemove() {
@@ -118,12 +101,8 @@ function displayRandomActivity(activityJson) {
     $('#results-list').removeClass('hidden');
     $('#js-form').addClass('hidden');
     $('#previous').removeClass('hidden');
+    $('#js-error-message').addClass('hidden');
  }
-
- function copyright() {
-    let d = new Date()
-    $('#copyright').text(`Copyright \u00A9 ${d.getFullYear()} Lou Zuniga https://github.com/louzuniga`)
-}
 
 function nextButtom () { 
     $('#next-activity').click(function() {
@@ -140,34 +119,41 @@ function nextButtom () {
     });
 }
 
+function copyright() {
+    let d = new Date()
+    $('#copyright').text(`Copyright \u00A9 ${d.getFullYear()} Lou Zuniga https://github.com/louzuniga`)
+}
+
 function watchForm () {
   $('form').submit(event => {
     event.preventDefault();
   });
-  copyright();
+ 
     $('.random-dog').on('click', () => {
     return getRandomDog();
     });
+
     $('.random-cat').on('click', () => {
     return getRandomCat();
     });
+
     $('form').on('click', '.random-activity',
         function () { 
          return getRandomActivity();
         });
+
     $('.random-advice').click(function() {
         return getRandomAdvice();
     });
-    // $('form').on( 'click', '.random-name', 
-    //     function() {
-    //         return getRandomName();
-    //     });
+
     $('#previous').on('click',() => {
         location.reload();
         $('#results').addClass('hidden');
         $('#js-form').removeClass('hidden');
     });
+    
     nextButtom();
+    copyright();
 }
 
 $(watchForm);
